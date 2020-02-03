d9f78cb73a87f90457fb890c4a9e14a49501c1c4
Dolosolow <Jose.Munoz07c@gmail.com>
2020-02-03 16:02:24 -0500

    Changed npm scripts and added webpack-merge package. Removed webpack default file for 3 webpack files, common + dev + prod.

---
+++ webpack.dev.js
@@ -0,0 +1,43 @@
+const { merge } = require("webpack-merge");
+const autoprefixer = require("autoprefixer");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+
+const common = require("./webpack.common");
+
+module.exports = merge(common, {
+  mode: "development",
+  entry: {
+    main: [
+      "webpack-hot-middleware/client?reload=true&timeout=1000",
+      "./src/public/js/forms/login-forms.js",
+      "./src/public/js/pages/index.js",
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: "development",
+              reloadAll: true,
+            },
+          },
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [autoprefixer()],
+            },
+          },
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+});
---
+++ webpack.prod.js
@@ -0,0 +1,35 @@
+const { merge } = require("webpack-merge");
+const autoprefixer = require("autoprefixer");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const common = require("./webpack.common");
+
+module.exports = merge(common, {
+  mode: "production",
+  devtool: "source-map",
+  entry: {
+    main: ["./src/public/js/forms/login-forms.js", "./src/public/js/pages/index.js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [autoprefixer()],
+            },
+          },
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [],
+});
