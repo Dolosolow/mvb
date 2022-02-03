e19e80fa29eb890917c6a4f0d75b0815bd992b2b
Dolosolow <Jose.Munoz07c@gmail.com>
2022-02-02 23:56:25 -0500

    added env port option to the server.

--- src/server/server.js
+++ src/server/server.js
@@ -1,43 +1,43 @@
-import dotenv from 'dotenv';
-import path from 'path';
-import { createServer } from 'http';
+import dotenv from "dotenv";
+import path from "path";
+import { createServer } from "http";
 
-import csrf from 'csurf';
-import express from 'express';
-import createSession from '@src/config/session';
-import helmetProtection from '@src/config/helmet';
+import csrf from "csurf";
+import express from "express";
+import createSession from "@src/config/session";
+import helmetProtection from "@src/config/helmet";
 
-import * as mongoDB from '@src/config/database';
-import createSocket from '@src/config/socket';
+import * as mongoDB from "@src/config/database";
+import createSocket from "@src/config/socket";
 
-import webpackServerConnect from '@src/config/wpServer';
+import webpackServerConnect from "@src/config/wpServer";
 
-import initialVisitorMW from '@src/middlewares/initialVisitor';
+import initialVisitorMW from "@src/middlewares/initialVisitor";
 
-import * as errorController from '@src/controllers/error.controller';
-import apiRoutes from '@src/routes/api';
-import adminRoutes from '@src/routes/admin';
-import storeRoutes from '@src/routes/store';
+import * as errorController from "@src/controllers/error.controller";
+import apiRoutes from "@src/routes/api";
+import adminRoutes from "@src/routes/admin";
+import storeRoutes from "@src/routes/store";
 // -------------
 // .env
 dotenv.config();
 // -------------
 // initializing express & connect to webpack middleware
 const server = express();
-webpackServerConnect(server, process.env.NODE_ENV === 'development');
+webpackServerConnect(server, process.env.NODE_ENV === "development");
 // -------------
-// creation of http server & socket.io 
+// creation of http server & socket.io
 const http = createServer(server);
 createSocket(http);
 // -------------
 // csurf protection
 const csrfProtection = csrf();
 
-server.use(express.static(path.resolve(__dirname, 'dist')));
+server.use(express.static(path.resolve(__dirname, "dist")));
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
-server.set('view engine', 'ejs');
-server.set('views', 'dist');
+server.set("view engine", "ejs");
+server.set("views", "dist");
 server.use(createSession(process.env.SESSION_SECRET));
 // -------------
 // helmetjs + csrf protection
@@ -48,16 +48,16 @@ server.use(helmetProtection);
 server.use(initialVisitorMW);
 // -------------
 // All routes being served
-server.use('/api', apiRoutes);
-server.use('/admin', adminRoutes);
-server.use('/', storeRoutes);
+server.use("/api", apiRoutes);
+server.use("/admin", adminRoutes);
+server.use("/", storeRoutes);
 server.use((req, res) => {
-  const error = new Error('Page Not Found');
+  const error = new Error("Page Not Found");
   error.status = 404;
   next(error);
 });
 server.use(errorController.get404);
 
 mongoDB.connect(() => {
-  http.listen(5001, () => console.log('🚀-lift off'));
+  http.listen(process.env.PORT || 5001, () => console.log("🚀-lift off"));
 });
