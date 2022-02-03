950c33bf0a55a48b1175cc115cd5da60d84d7eec
Dolosolow <Jose.Munoz07c@gmail.com>
2022-02-03 16:26:11 -0500

    Implemented the webpack dev configuration from webpack.dev.js

--- src/config/wpServer.js
+++ src/config/wpServer.js
@@ -1,22 +1,24 @@
-import path from 'path';
-import webpack from 'webpack';
-import config from '../../webpack.config';
-import webpackDM from 'webpack-dev-middleware';
-import webpackHM from 'webpack-hot-middleware';
+import path from "path";
+import webpack from "webpack";
+import config from "../../webpack.dev";
+import webpackDM from "webpack-dev-middleware";
+import webpackHM from "webpack-hot-middleware";
 
 const webpackServerConnect = (server, inDevelopment) => {
-  if(inDevelopment) {
+  if (inDevelopment) {
     const compiler = webpack(config);
-    
-    server.use(webpackDM(compiler, {
-      contentBase: path.resolve(__dirname, 'dist'),
-      hot: true,
-      host: `localhost`,
-      publicPath: config.output.publicPath
-    }))
+
+    server.use(
+      webpackDM(compiler, {
+        contentBase: path.resolve(__dirname, "dist"),
+        hot: true,
+        host: `localhost`,
+        publicPath: config.output.publicPath,
+      })
+    );
 
     server.use(webpackHM(compiler));
   }
-}
+};
 
 export default webpackServerConnect;
